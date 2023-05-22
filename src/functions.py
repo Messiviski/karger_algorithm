56f502b63fd6f877a63dffdbaf85f308708a5aa9
Messias <messiasolimpio@hotmail.com>
2023-05-22 10:27:00 -0300

    teste

--- src/functions.py
+++ src/functions.py
@@ -1,3 +1,4 @@
+import numpy as np
 from graph import Graph
 from naive import Naive
 from karger import Karger
@@ -8,12 +9,11 @@ import matplotlib.pyplot as plt
 def analyse(graph: Graph, min_cut: int, n_exec: int, n_iter: int) -> None:
     karger = Karger(graph)
     naive = Naive(graph)
-    #    general_prob = 0
 
-    plt.axis([0, n_iter, 0, 1])
+    probabilties = []
+    iterations = []
 
-    for i in range(1000):
-        prob_karger = prob_naive = 0.0
+    for i in range(4):
         iterations_karger = iterations_naive = 0
 
         for j in range(1, n_exec + 1):
@@ -26,17 +26,18 @@ def analyse(graph: Graph, min_cut: int, n_exec: int, n_iter: int) -> None:
             if result == min_cut:
                 iterations_naive += 1
 
-        prob_karger = iterations_karger / n_exec
-        prob_naive = iterations_naive / n_exec
+        iterations.append([iterations_karger, iterations_naive])
+        probabilties.append(
+            [iterations_karger / n_exec, iterations_naive / n_exec]
+        )
 
-        plt.scatter(prob_karger, iterations_karger, cmap='r')
-        plt.scatter(prob_naive, iterations_naive, cmap='b')
-        plt.pause(0.05)
-        #   print(prob_karger)
-        #   print(prob_naive)
-        #   print("============================")
+        n_iter += 20
 
-        n_iter += 10
+        print(f'=========== {i} ===========')
+        print(f'Karger  {iterations_karger / n_exec} - {n_iter}')
+        print(f'Naive   {iterations_naive / n_exec} - {n_iter}')
+
+    plot(iterations, probabilties)
 
 
 def run_algorithm(algorithm_instance: Algorithm, n_iter: int) -> int:
@@ -48,3 +49,16 @@ def run_algorithm(algorithm_instance: Algorithm, n_iter: int) -> int:
             best_result = new_result
 
     return best_result
+
+
+def plot(x: list[int, int], y: list[float, float]) -> None:
+    np_probabilities = np.array(y)
+    np_iterations = np.array(x)
+
+    # Karger
+    plt.plot(np_iterations[0:, 0], np_probabilities[0:, 0])
+
+    # Naive
+    plt.plot(np_iterations[0:, 1], np_probabilities[0:, 1])
+
+    plt.show()
