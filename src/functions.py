7cf46afc4fbb6f87065d93710a97c86ba60025dc
Messias <messiasolimpio@hotmail.com>
2023-05-28 23:24:50 -0300

    fix: stop condition improved

--- src/functions.py
+++ src/functions.py
@@ -6,23 +6,16 @@ import matplotlib.pyplot as plt
 
 
 def analyse(
-            graph: Graph,
-            min_cut: int,
-            n_exec: int,
-            n_iter: int,
-            e: float = 0.01
-        ) -> None:
-    karger = Karger(graph)
-    naive = Naive(graph)
+        graph: Graph, min_cut: int, n_exec: int, n_iter: int,
+        e: float = 0.01) -> None:
 
-    KARGER = 0
-    NAIVE = 1
-
-    probabilities = []
-    iterations = []
+    karger, naive = Karger(graph), Naive(graph)
+    KARGER, NAIVE = 0, 1
+    probabilities, iterations = [], []
+    karger_stop_condition = False
+    stop_count = 0
 
     while True:
-        prob_karger = prob_naive = 0
         iterations_karger = iterations_naive = 0
 
         for j in range(1, n_exec + 1):
@@ -34,18 +27,23 @@ def analyse(
             if results[NAIVE] == min_cut:
                 iterations_naive += 1
 
-        prob_karger = iterations_karger / n_exec
-        prob_naive = iterations_naive / n_exec
-
-        changed = has_changed(probabilities, prob_naive, e)
-
         iterations.append(n_iter)
-        probabilities.append([prob_karger, prob_naive])
+        probabilities.append(
+            calculate_probabilities(iterations_karger, iterations_naive, n_exec)
+        )
 
-        if not changed:
+        if probabilities[-1][KARGER] == 1.0 and karger_stop_condition is False:
+            karger_stop_condition = True
+
+        if check_stop_conditions(probabilities, e):
+            stop_count += 1
+
+        if stop_count == 5:
             break
 
         n_iter += 20
+
+        print(f'Latest Probabilities -> {probabilities[-1]}')
 
     plot(iterations, probabilities)
 
@@ -67,13 +65,37 @@ def iterate(karger: Karger, naive: Naive, n_iter: int) -> list[int, int]:
     return [karger_best_result, naive_best_result]
 
 
-def has_changed(probabilities: list, probability: float, e: float) -> bool:
-    if len(probabilities) == 0:
+def calculate_probabilities(karger: int, naive: int, n_exec: int) -> None:
+    return [karger / n_exec, naive / n_exec]
+
+
+def check_stop_conditions(probabilities: list, e: float) -> None:
+    if len(probabilities) <= 1:
+        return False
+
+    new_probabilities = np.array(probabilities)
+    KARGER, NAIVE = 0, 1
+
+    current_odds, last_probabilities = new_probabilities[-2:]
+
+    if current_odds[KARGER] == 1.0 and current_odds[NAIVE] == 1.0:
         return True
 
-    last_naive_probability = probabilities[-1][-1]
+    karger_odds, naive_odds = get_latest_probabilities(new_probabilities)
 
-    if (probability - last_naive_probability) < e:
+    if not has_changed(karger_odds, e) and not has_changed(naive_odds, e):
+        return True
+
+
+def get_latest_probabilities(probabilities: list) -> list:
+    last_probabilities = probabilities[-2:]
+    return [last_probabilities[0:, 0], last_probabilities[0:, 1]]
+
+
+def has_changed(probabilities: list, e: float):
+    LAST, BEFORE_LAST = [1, 0]
+
+    if (probabilities[LAST] - probabilities[BEFORE_LAST]) < e:
         return False
 
     return True
